fed4e8fdf035e8eaa75fb5c7bccd5bfe8216e68a
shariThasneem <sharibathasneem@gmail.com>
2023-11-01 16:41:43 +0530

    nav style updated

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const ham = document.querySelector(".ham");
 var dropDown = document.querySelector(".dropDown");
+var drop = document.querySelector(".drop");
 const menuIcon = document.getElementById("menuIcon");
 const computedStyle = getComputedStyle(dropDown);
 const displayValue = computedStyle.getPropertyValue("display");
 const cancel = document.querySelector(".cancel");
+const pricing_card = document.querySelector(".pricing-card");
 
 menuIcon.addEventListener("click", () => {
 	if (displayValue == "none") {
 		dropDown.style.display = "flex";
+		drop.style.display = "flex";
 	}
 });
 cancel.addEventListener("click", () => {
